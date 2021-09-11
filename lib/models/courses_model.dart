86c1326adae7a3a3d4fd1e2ff4f3ddb5b58e0bdb
Zubair Sattar <zubairsattar38@gmail.com>
2021-09-11 13:20:44 +0500

    Add Models

--- lib/models/courses_model.dart
+++ lib/models/courses_model.dart
@@ -1,23 +1,23 @@
 import 'dart:convert';
 
 class Courses {
-   List<dynamic> ? content;
-   String ?pageable = {} as String;
-   int ?totalElements;
-   bool ?last;
-   int ?totalPages;
-   bool ?first;
-   String sort = {} as String;
-   int ?numberOfElements;
-   int ?size;
-   int ?number;
-   bool ?empty;
-
-  Courses({this.content,this.totalElements,this.last,this.totalPages,this.first,required this.sort,this.numberOfElements,this.size,this.number,this.empty,this.pageable, List headers});
+   List<dynamic>  content;
+   dynamic pageable = {};
+   dynamic totalElements;
+   // ignore: unnecessary_question_mark
+   dynamic last;
+   dynamic totalPages;
+   dynamic first;
+   dynamic sort = {};
+   dynamic numberOfElements;
+   dynamic size;
+   dynamic number;
+   dynamic empty;
+  Courses({required this.content,required this.totalElements,this.last,this.totalPages,this.first,required this.sort,this.numberOfElements,this.size,this.number,this.empty,required this.pageable});
 
   Map<String, dynamic> toMap() {
     return {
-      'content': content?.map((x) => x.toMap()).toList(),
+      'content': content.map((x) => x.toMap()).toList(),
       'totalElements': totalElements,
       'last': last,
       'totalPages': totalPages,
@@ -29,18 +29,20 @@ class Courses {
     };
   }
 
-  factory Courses.fromJson(map) {
+  factory Courses.fromJson(Map<String, dynamic> map) {
     return Courses(
-      content: List<dynamic>.from(map['content'] as List<dynamic>),
+      content: List<dynamic> .from(map['content'] as List<dynamic>),
+      totalElements: map['totalElements'],
       last: map['last'],
-
-      totalElements: map['totalElements']
       totalPages: map['totalPages'],
       first: map['first'],
       numberOfElements: map['numberOfElements'],
       size: map['size'],
       number: map['number'],
-      empty: map['empty'], sort: '',
+      empty: map['empty'],
+      pageable: map['pageable'],
+      sort: map['sort'],
+
     );
   }
 

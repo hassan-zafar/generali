62fa29d9b545a7e394749058eb4983e9f28a6eb3
askabdulhaseeb <askabdulhaseeb@gmail.com>
2021-09-17 03:27:04 +0500

    Courses Detail screen UI completed

---
+++ lib/screens/course_detail/course_detail.dart
@@ -0,0 +1,228 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+import 'package:generali/model/course.dart';
+import 'package:generali/utilities/custom_colors.dart';
+import 'package:generali/utilities/utilities.dart';
+
+class CourseDetail extends StatelessWidget {
+  const CourseDetail({required this.course, Key? key}) : super(key: key);
+  final Courses course;
+
+  @override
+  Widget build(BuildContext context) {
+    final Size _size = MediaQuery.of(context).size;
+    return SafeArea(
+      child: Scaffold(
+        body: Column(
+          children: <Widget>[
+            AspectRatio(
+              aspectRatio: 16 / 9,
+              child: Container(
+                color: Colors.black38,
+                alignment: Alignment.center,
+                child: const Text('Video Player Area'),
+              ),
+            ),
+            Expanded(
+              child: Stack(
+                children: <Widget>[
+                  _acceptButton(context),
+                  SizedBox(
+                    width: double.infinity,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Padding(
+                          padding: EdgeInsets.all(Utilities.padding),
+                          child: Text(
+                            course.title!,
+                            style: const TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                        const Divider(thickness: 2),
+                        Padding(
+                          padding:
+                              EdgeInsets.symmetric(vertical: Utilities.padding),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: <Widget>[
+                              _IcoicTitleButton(
+                                icon: Icons.access_time_rounded,
+                                title: '3.00',
+                                onTap: () {},
+                              ),
+                              _IcoicTitleButton(
+                                icon: CupertinoIcons.eye_solid,
+                                title: 'Visualizations',
+                                onTap: () {},
+                              ),
+                              _IcoicTitleButton(
+                                icon: Icons.link_rounded,
+                                title: 'Tipo Productos',
+                                onTap: () {},
+                              ),
+                            ],
+                          ),
+                        ),
+                        const Divider(thickness: 2),
+                        Padding(
+                          padding: EdgeInsets.symmetric(
+                            horizontal: Utilities.padding,
+                          ),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              _consultButton(context),
+                              _rating(context),
+                              const SelectableText(
+                                  'A paragraph is a group of related sentences that support one main idea. In general, paragraphs consist of three parts: the topic sentence, body sentences, and the concluding or the bridge sentence to the next paragraph or section'),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Positioned _acceptButton(BuildContext context) {
+    return Positioned(
+      left: Utilities.padding,
+      right: Utilities.padding,
+      bottom: Utilities.padding,
+      child: Container(
+        width: double.infinity,
+        alignment: Alignment.center,
+        padding: EdgeInsets.all(Utilities.padding),
+        margin: EdgeInsets.all(Utilities.padding),
+        decoration: BoxDecoration(
+          color: Theme.of(context).primaryColor,
+          borderRadius: BorderRadius.circular(
+            Utilities.searchBorderRadius,
+          ),
+        ),
+        child: const Text(
+          'Accede al soports',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Row _rating(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        IconButton(
+          padding: EdgeInsets.zero,
+          splashRadius: 20,
+          onPressed: () {},
+          icon: Icon(
+            CupertinoIcons.hand_thumbsup,
+            color: Theme.of(context).primaryColor,
+          ),
+        ),
+        IconButton(
+          padding: EdgeInsets.zero,
+          splashRadius: 20,
+          onPressed: () {},
+          icon: Icon(
+            CupertinoIcons.hand_thumbsdown,
+            color: Theme.of(context).primaryColor,
+          ),
+        ),
+        const Spacer(),
+        RatingBar(
+          itemSize: 20,
+          wrapAlignment: WrapAlignment.end,
+          initialRating: 3,
+          minRating: 1,
+          maxRating: 5,
+          allowHalfRating: true,
+          onRatingUpdate: (double rating) {
+            // TODO: on change
+          },
+          ratingWidget: RatingWidget(
+            full: const Icon(Icons.star, color: Colors.amber),
+            half: const Icon(Icons.star_border_purple500, color: Colors.amber),
+            empty: Icon(Icons.star_border, color: CustomColors.darkGrey),
+          ),
+        ),
+      ],
+    );
+  }
+
+  GestureDetector _consultButton(BuildContext context) {
+    return GestureDetector(
+      onTap: () {},
+      child: Container(
+        padding: EdgeInsets.symmetric(
+          horizontal: Utilities.padding,
+          vertical: Utilities.padding / 2,
+        ),
+        margin: EdgeInsets.symmetric(vertical: Utilities.padding),
+        decoration: BoxDecoration(
+          color: Theme.of(context).primaryColor,
+          borderRadius: BorderRadius.circular(
+            Utilities.buttonBorderRadius,
+          ),
+        ),
+        child: const Text(
+          'Consultado',
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _IcoicTitleButton extends StatelessWidget {
+  const _IcoicTitleButton({
+    required this.icon,
+    required this.title,
+    required this.onTap,
+    Key? key,
+  }) : super(key: key);
+  final IconData icon;
+  final String title;
+  final VoidCallback onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Column(
+        children: <Widget>[
+          CircleAvatar(
+            radius: 20,
+            backgroundColor: Theme.of(context).primaryColor,
+            child: Icon(icon),
+          ),
+          Text(
+            title,
+            style: TextStyle(color: CustomColors.lightGrey),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/screens/main_scren/pages/aprenda/courses_tile_widget.dart
+++ lib/screens/main_scren/pages/aprenda/courses_tile_widget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:generali/model/course.dart';
+import 'package:generali/screens/course_detail/course_detail.dart';
 import 'package:generali/utilities/custom_colors.dart';
 import 'package:generali/utilities/custom_images.dart';
 import 'package:generali/utilities/utilities.dart';
@@ -16,41 +17,51 @@ class CoursesTileWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 100,
-      width: double.infinity,
-      margin: EdgeInsets.symmetric(
-        vertical: Utilities.padding / 2,
-        horizontal: Utilities.padding,
-      ),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(Utilities.searchBorderRadius),
-        color: Colors.white,
-        boxShadow: <BoxShadow>[
-          BoxShadow(
-            color: Colors.black.withOpacity(0.2),
-            spreadRadius: 2,
-            blurRadius: 4,
-            offset: const Offset(1, 1),
+    return GestureDetector(
+      onTap: () {
+        Navigator.of(context).push(
+          MaterialPageRoute<CourseDetail>(
+            builder: (BuildContext context) => CourseDetail(course: course),
           ),
-        ],
-      ),
-      child: Row(
-        children: <Widget>[
-          _courseImage(),
-          _courseInfo(context),
-          if (showFavIcon)
-            IconButton(
-              splashRadius: 10,
-              onPressed: () {},
-              icon: (course.isFav!)
-                  ? Icon(Icons.favorite, color: Theme.of(context).primaryColor)
-                  : Icon(Icons.favorite, color: CustomColors.lightGrey),
-            )
-          else
-            const SizedBox(),
-          const SizedBox(width: 10),
-        ],
+        );
+      },
+      child: Container(
+        height: 100,
+        width: double.infinity,
+        margin: EdgeInsets.symmetric(
+          vertical: Utilities.padding / 2,
+          horizontal: Utilities.padding,
+        ),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(Utilities.searchBorderRadius),
+          color: Colors.white,
+          boxShadow: <BoxShadow>[
+            BoxShadow(
+              color: Colors.black.withOpacity(0.2),
+              spreadRadius: 2,
+              blurRadius: 4,
+              offset: const Offset(1, 1),
+            ),
+          ],
+        ),
+        child: Row(
+          children: <Widget>[
+            _courseImage(),
+            _courseInfo(context),
+            if (showFavIcon)
+              IconButton(
+                splashRadius: 10,
+                onPressed: () {},
+                icon: (course.isFav!)
+                    ? Icon(Icons.favorite,
+                        color: Theme.of(context).primaryColor)
+                    : Icon(Icons.favorite, color: CustomColors.lightGrey),
+              )
+            else
+              const SizedBox(),
+            const SizedBox(width: 10),
+          ],
+        ),
       ),
     );
   }
